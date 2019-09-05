527162bd60fc13928667fe3a3faaf495db8b5db7
Lucas Martins <dev.lucasmartins@gmail.com>
2019-09-05 19:01:07 -0300

    ae

--- default_settings.py
+++ default_settings.py
@@ -14,7 +14,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 
 # JWT
-JWT_SECRET_KEY = os.urandom(24)
+JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', os.urandom(24)) 
 JWT_EXPIRATION_DELTA = dt.timedelta(seconds=3600) # a datetime.timedelta indicating how long tokens are valid for.
 JWT_ACCESS_TOKEN_EXPIRES = dt.timedelta(seconds=3600)
 JWT_REFRESH_TOKEN_EXPIRES = dt.timedelta(days=30)
